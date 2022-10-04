17a6738674783fef837ca21bc953d53319260b01
Samoylenko <samoylenk0.alexander@yandex.ru>
2022-10-04 20:53:01 +0300

    Final version of the programm

--- Program.cs
+++ Program.cs
@@ -6,3 +6,31 @@
 
 
 string[] arrString = {"dog", "cat", "man", "home", "boy", "girls", "day", "joke", "bat", "sun", "month"};
+int newArrayLength = 0;
+
+Console.WriteLine("Начальный массив: ");
+Console.WriteLine($"{String.Join("; ",arrString )}");
+
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length <= 3)
+    {
+        newArrayLength++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Финальный массив:");
+
+string[] newArray = new string[newArrayLength];
+int k = 0;
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length <= 3)
+    {
+        newArray[k] = arrString[i];
+        k++;
+    }
+}
+
+Console.WriteLine($"{String.Join("; ",newArray )}");
